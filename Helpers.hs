1c7af893763f74312affce5c88ca6756d82cd6db
Colin McDonald <colinmcd0731@gmail.com>
2022-11-08 11:32:30 -0500

    Add helpers file

---
+++ Helpers.hs
@@ -0,0 +1,20 @@
+module Helpers where
+import Struct
+import Parse
+import Lex
+
+readTerms :: (Term -> String) -> IO ()
+readTerms f =
+  lines <$> getContents >>= \ ls ->
+  foldr (\ s rest i -> putStrLn (either id f (lexStrL s i >>= parseOut parseTerm1)) >> rest (succ i)) (\ _ -> return ()) ls 1
+
+pairs :: [a] -> [(a, a)]
+pairs (a1 : a2 : as) = (a1, a2) : pairs as
+pairs _ = []
+
+readTwoTerms :: (Term -> Term -> String) -> IO ()
+readTwoTerms f =
+  map (\ (st, su) -> st >>= \ t -> su >>= \ u -> return (t, u)) <$> pairs <$>
+    map (\ (i, s) -> lexStrL s i >>= parseOut parseTerm1) <$>
+    zip [1..] <$> lines <$> getContents >>=
+  foldr (\ ts rest -> putStrLn (either id (uncurry f) ts) >> rest) (return ())
