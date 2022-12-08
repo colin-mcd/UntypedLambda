570139fe73ff7ceccd0baeeb47eae5097ec7c462
Colin McDonald <colinmcd0731@gmail.com>
2022-12-08 10:46:21 -0500

    Pretty-printing of DeBruijn terms

--- DeBruijn.hs
+++ DeBruijn.hs
@@ -1,4 +1,6 @@
 module DeBruijin where
+import Data.List (elemIndex)
+import Struct
 
 -- De Bruijn-indexed term
 data DBTm =
@@ -11,3 +13,36 @@ toDBTm' xs (Var x) = maybe (error "This shouldn't happen") DBVar (elemIndex x xs
 toDBTm' xs (App t u) = DBApp (toDBTm' xs t) (toDBTm' xs u)
 toDBTm' xs (Lam x t) = DBLam (toDBTm' (x : xs) t)
 
+lamToDB :: Term -> DBTm
+lamToDB = toDBTm' []
+
+dBToLam :: DBTm -> Term
+dBToLam = h 0 where
+  h n (DBVar x) = Var ('x' : show (n - x))
+  h n (DBApp t u) = App (h n t) (h n u)
+  h n (DBLam t) = Lam ('x' : show n) (h (succ n) t)
+
+
+
+------------------------------
+-- Printing
+------------------------------
+
+-- Does printing term t need parens, given history s?
+needParensDB ShowNone _ = False
+needParensDB s (DBVar _) = False
+needParensDB ShowLam  (DBApp _ _) = False
+needParensDB ShowAppL (DBApp _ _) = False
+needParensDB ShowAppR (DBApp _ _) = True
+needParensDB ShowLam  (DBLam _) = False
+needParensDB ShowAppL (DBLam _) = True
+needParensDB ShowAppR (DBLam _) = True -- abc (\x. x)
+
+showDBTermh (DBVar x) = show x
+showDBTermh (DBApp t u) = showDBTerm ShowAppL t ++ " " ++ showDBTerm ShowAppR u
+showDBTermh (DBLam t) = "\\" ++ showDBTerm ShowLam t
+
+showDBTerm s t = doIf (needParensDB s t) parens (showDBTermh t)
+
+instance Show DBTm where
+  show = showDBTerm ShowNone
