3295d93271dc76eb343dd719ffe51822d5d5f141
Colin McDonald <colinmcd0731@gmail.com>
2025-04-14 12:48:24 -0400

    Expose contents externally

--- UntypedLambda.hs
+++ UntypedLambda.hs
@@ -1,15 +1,15 @@
-module UntypedLambda where
-import qualified ANF
-import qualified Bohm
-import qualified CPS
-import qualified DeBruijn
-import qualified Fresh
-import qualified Helpers
-import qualified Inline
-import qualified Lex
-import qualified Parse
-import qualified Reduce
-import qualified SCC
-import qualified SKI
-import qualified Struct
-import qualified Subst
+module UntypedLambda (module ANF, module Bohm, module CPS, module Fresh, module Helpers, module Inline, module Lex, module Parse, module Reduce, module SCC, module SKI, module Struct, module Subst) where
+import ANF
+import Bohm
+import CPS
+--import DeBruijn
+import Fresh
+import Helpers
+import Inline
+import Lex
+import Parse
+import Reduce
+import SCC
+import SKI
+import Struct
+import Subst
